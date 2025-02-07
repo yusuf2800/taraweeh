b44c77b347c160ca518d7ea49bc77808bbc1285d
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-02-07 23:10:47 +0000

    fixed respnsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@ const Header = () => {
   const difference1 = Math.ceil(
     (new Date("2025-02-28") - new Date()) / (1000 * 60 * 60 * 24)
   );
-  const difference2 = Math.ceil(
-    (new Date("2025-02-29") - new Date()) / (1000 * 60 * 60 * 24)
-  );
 
   return (
     <div>
@@ -14,16 +11,19 @@ const Header = () => {
           style={{
             fontSize: "37px",
             position: "fixed",
+            top: "0",
             backgroundColor: "#ffecd4",
-            width: "100vw",
+            left: "0",
+            right: "0",
             display: "flex",
+            flexWrap: "wrap",
             alignItems: "center",
             justifyContent: "center",
             height: "70px",
             boxShadow: "0 4px 5px rgba(0, 0, 0, 0.3",
           }}
         >
-          Days left: {difference1}/{difference2}
+          Days left: {difference1}/{difference1 + 1}
         </label>
       </div>
     </div>
