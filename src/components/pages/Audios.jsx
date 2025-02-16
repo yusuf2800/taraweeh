a7094b45ff89e8ac7fa09f298e2e0537cb3bf058
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-02-16 11:03:38 +0000

    adding reciters

--- src/components/pages/Audios.jsx
+++ src/components/pages/Audios.jsx
@@ -2,11 +2,37 @@ import { getAudios } from "../../QuranApi.js";
 import "./Audios.css";
 
 const Audios = () => {
+  getAudios(5, 10).then((res) => console.log(res));
+
+  const reciters = [
+    {
+      name: "Mishary Rashid Alafasy",
+      arabicName: "مشاري بن راشدالعفاسي",
+      url: "https://api2.quran-pro.com/images/mishary-rashid-alafasy/mishary-rashid-alafasy-medium.webp?version=1686738242860",
+    },
+    {
+      name: "Yasser Al-Dosari",
+      arabicName: "ياسر الدوسري",
+      url: "https://api2.quran-pro.com/images/yasser-al-dosari/yasser-al-dosari-medium.webp?version=1686734240565",
+    },
+    {
+      name: "Abdul Rahman Al Soudais",
+      arabicName: "عبد الرحمن السديس",
+      url: "https://api2.quran-pro.com/images/abdul-rahman-al-soudais/abdul-rahman-al-soudais-medium.webp?version=1686737500276",
+    },
+  ];
+
   return (
     <div className="parent-container">
-
+      {reciters.map((reciter) => (
+        <div className="reciter-wrapper">
+          <img src={reciter.url} className="img" />
+          <p className="author">{reciter.name}</p>
+          <p className="arabic">{reciter.arabicName}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Audios
+export default Audios;
