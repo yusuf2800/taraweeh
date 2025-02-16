5fd25bceb9528d1036358fa848e2cc45fddf6841
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-02-16 22:26:45 +0000

    added night theme button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -21,7 +21,7 @@ const Body = () => {
       <Link className="audios" key={Math.random()} to="/audios">
         Level Up Your Recitation
       </Link>
-      <button className="theme-switch" onClick={toggle}>
+      <button className="theme-switch">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
