3871a369a52adf360882706339e4fe1e1d8b8b51
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-02-20 14:24:17 +0000

    fixed responsiveness

--- src/components/pages/Audios.jsx
+++ src/components/pages/Audios.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import "./Audios.css";
 
 const Audios = () => {
-
   const reciters = [
     {
       name: "Mishary Rashid Alafasy",
@@ -16,7 +15,6 @@ const Audios = () => {
       url: "https://api2.quran-pro.com/images/yasser-al-dosari/yasser-al-dosari-medium.webp?version=1686734240565",
       goto: "",
     },
-
   ];
 
   return (
@@ -37,6 +35,20 @@ const Audios = () => {
           </Link>
         ))}
       </div>
+      <Link key={Math.random()} to="/">
+        <button className="home">
+          <svg
+            className="back"
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            viewBox="0 -960 960 960"
+            width="30px"
+            fill="rgb(22, 60, 94)"
+          >
+            <path d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z" />
+          </svg>
+        </button>
+      </Link>
     </div>
   );
 };
