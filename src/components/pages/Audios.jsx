7a8cb1aa01ba103665f0dfa6d3f263e535c7729a
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-03-05 19:02:13 +0000

    added some modal content

--- src/components/pages/Audios.jsx
+++ src/components/pages/Audios.jsx
@@ -71,8 +71,8 @@ const Audios = () => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
-          width="30px"
-          height="30px"
+          width="27px"
+          height="27px"
           fill="rgb(22, 60, 94)"
         >
           <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z" />
@@ -99,7 +99,7 @@ const Audios = () => {
           The audios are still being added
           <br />
           <br />
-          More reciters will be added soon 
+          More reciters will be added soon
           <br />
           Please make sure to share this website for more hasanat
           <br />
@@ -107,6 +107,9 @@ const Audios = () => {
           <a href="mailto:yusufpatel2800@gmail.com" className="email">
             Email me for suggestions/improvements
           </a>
+          <br />
+          <br />
+          <span className="arabic">جزاك الله خيرا</span>
         </p>
       </dialog>
     </div>
